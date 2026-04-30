d8c306c782079df132688ca49bb1e14aca6f966e
Karla Georgina Chiw Lara <karla.chiw@ntsdata.com.mx>
2026-04-30 03:43:04 -0600

    Corrigiendo nombre de carpeta archivos_locales

--- datos.js
+++ datos.js
@@ -1,6 +1,6 @@
 const listaDeJuegos = [
 
-  { "titulo": "Crash Bandicoot 1", "consola": "psx", "imagen": "imagenes/image.png", "url": "arcadelocal/archivoslocales/crash.txt" },
+  { "titulo": "Crash Bandicoot 1", "consola": "psx", "imagen": "imagenes/image.png", "url": "arcadelocal/archivos_locales/crash.txt" },
   { "titulo": "Resident Evil 3", "consola": "psx", "imagen": "imagenes/resident.png", "url": "http://TU_IP_PC:8000/psx/re3.bin" },
   { "titulo": "Metal Gear Solid", "consola": "psx", "imagen": "imagenes/metal.png", "url": "http://TU_IP_PC:8000/psx/mgs.bin" },
   { "titulo": "Tekken 3", "consola": "psx", "imagen": "imagenes/tekken.png", "url": "http://TU_IP_PC:8000/psx/tekken3.bin" },
@@ -11,7 +11,7 @@ const listaDeJuegos = [
   { "titulo": "Spyro the Dragon", "consola": "psx", "imagen": "imagenes/spyro.png", "url": "http://TU_IP_PC:8000/psx/spyro.bin" },
   { "titulo": "Spider-Man (2000)", "consola": "psx", "imagen": "imagenes/spyder.png", "url": "http://TU_IP_PC:8000/psx/spiderman.bin" },
 
-  { "titulo": "Metal Slug X", "consola": "fbneo", "imagen": "imagenes/slug.png", "url": "metalx.txt" },
+  { "titulo": "Metal Slug X", "consola": "fbneo", "imagen": "imagenes/slug.png", "url": "arcadelocal/archivos_locales/metalx.txt" },
   { "titulo": "The King of Fighters 98", "consola": "fbneo", "imagen": "imagenes/98.png", "url": "http://TU_IP_PC:8000/arcade/kof98.zip" },
   { "titulo": "Street Fighter II", "consola": "fbneo", "imagen": "imagenes/street.png", "url": "http://TU_IP_PC:8000/arcade/sf2.zip" },
   { "titulo": "Pac-Man", "consola": "fbneo", "imagen": "imagenes/pacman.png", "url": "http://TU_IP_PC:8000/arcade/pacman.zip" },
@@ -20,7 +20,7 @@ const listaDeJuegos = [
   { "titulo": "Sunset Riders", "consola": "fbneo", "imagen": "imagenes/sunset.png", "url": "http://TU_IP_PC:8000/arcade/ssriders.zip" },
   { "titulo": "Snow Bros", "consola": "fbneo", "imagen": "imagenes/snow.png", "url": "http://TU_IP_PC:8000/arcade/snowbros.zip" },
   { "titulo": "Contra", "consola": "fbneo", "imagen": "imagenes/contra.png", "url": "http://TU_IP_PC:8000/arcade/contra.zip" },
-  { "titulo": "Double Dragon", "consola": "fbneo", "imagen": "imagenes/double.png", "url": "double.txt" },
+  { "titulo": "Double Dragon", "consola": "fbneo", "imagen": "imagenes/double.png", "url": "arcadelocal/archivos_locales/double.txt" },
 
   { "titulo": "Halo 2", "consola": "xbox", "imagen": "imagenes/halo2.png", "url": "http://TU_IP_PC:8000/xbox/halo2.iso" },
   { "titulo": "Ninja Gaiden Black", "consola": "xbox", "imagen": "imagenes/ninja.png", "url": "http://TU_IP_PC:8000/xbox/ngb.iso" },
@@ -36,61 +36,61 @@ const listaDeJuegos = [
         "titulo": "Super Mario 64",
         "consola": "n64",
         "imagen": "imagenes/mario.png",
-        "url": "mario64.z64"
+        "url": "arcadelocal/archivos_locales/mario64.z64"
     },
     {
         "titulo": "The Legend of Zelda: Ocarina of Time",
         "consola": "n64",
         "imagen": "imagenes/ocarina.png",
-        "url": "zelda.txt"
+        "url": "arcadelocal/archivos_locales/zelda.txt"
     },
     {
         "titulo": "GoldenEye 007",
         "consola": "n64",
         "imagen": "imagenes/golden.png",
-        "url": "007.z64"
+        "url": "arcadelocal/archivos_locales/007.z64"
     },
     {
         "titulo": "Mario Kart 64",
         "consola": "n64",
         "imagen": "imagenes/kart.png",
-        "url": "mk64.z64"
+        "url": "arcadelocal/archivos_locales/mk64.z64"
     },
     {
         "titulo": "Star Fox 64",
         "consola": "n64",
         "imagen": "imagenes/fox.png",
-        "url": "starfox64.z64"
+        "url": "arcadelocal/archivos_locales/starfox64.z64"
     },
     {
         "titulo": "Banjo-Kazooie",
         "consola": "n64",
         "imagen": "imagenes/banjo.png",
-        "url": "banjo.z64"
+        "url": "arcadelocal/archivos_locales/banjo.z64"
     },
     {
         "titulo": "Super Smash Bros.",
         "consola": "n64",
         "imagen": "imagenes/super.png",
-        "url": "smash64.z64"
+        "url": "arcadelocal/archivos_locales/smash64.z64"
     },
     {
         "titulo": "Perfect Dark",
         "consola": "n64",
         "imagen": "imagenes/perfect.png",
-        "url": "pdark.z64"
+        "url": "arcadelocal/archivos_locales/pdark.z64"
     },
     {
         "titulo": "Resident Evil 2",
         "consola": "n64",
         "imagen": "imagenes/re2.png",
-        "url": "re2.z64"
+        "url": "arcadelocal/archivos_locales/re2.z64"
     },
     {
         "titulo": "Conker's Bad Fur Day",
         "consola": "n64",
         "imagen": "imagenes/conker.png",
-        "url": "conker.z64"
+        "url": "arcadelocal/archivos_locales/conker.z64"
     },
 
     // --- GAMECUBE ---
@@ -98,60 +98,60 @@ const listaDeJuegos = [
         "titulo": "Super Smash Bros. Melee",
         "consola": "gamecube",
         "imagen": "imagenes/mele.png",
-        "url": "ssbm.iso"
+        "url": "arcadelocal/archivos_locales/ssbm.iso"
     },
     {
         "titulo": "The Legend of Zelda: The Wind Waker",
         "consola": "gamecube",
         "imagen": "imagenes/win.png",
-        "url": "zww.iso"
+        "url": "arcadelocal/archivos_locales/zww.iso"
     },
     {
         "titulo": "Metroid Prime",
         "consola": "gamecube",
         "imagen": "imagenes/metroid.png",
-        "url": "metroid_prime.iso"
+        "url": "arcadelocal/archivos_locales/metroid_prime.iso"
     },
     {
         "titulo": "Resident Evil 4",
         "consola": "gamecube",
         "imagen": "imagenes/re4.png",
-        "url": "re4.iso"
+        "url": "arcadelocal/archivos_locales/re4.iso"
     },
     {
         "titulo": "Mario Kart: Double Dash!!",
         "consola": "gamecube",
         "imagen": "imagenes/dash.png",
-        "url": "kart.txt"
+        "url": "arcadelocal/archivos_locales/kart.txt"
     },
     {
         "titulo": "Luigi's Mansion",
         "consola": "gamecube",
         "imagen": "imagenes/luigi.png",
-        "url": "luigi.iso"
+        "url": "arcadelocal/archivos_locales/luigi.iso"
     },
     {
         "titulo": "Star Fox Adventures",
         "consola": "gamecube",
         "imagen": "imagenes/adve.png",
-        "url": "sf_adv.iso"
+        "url": "arcadelocal/archivos_locales/sf_adv.iso"
     },
     {
         "titulo": "Eternal Darkness",
         "consola": "gamecube",
         "imagen": "imagenes/eternal.png",
-        "url": "edarkness.iso"
+        "url": "arcadelocal/archivos_locales/edarkness.iso"
     },
     {
         "titulo": "Pikmin",
         "consola": "gamecube",
         "imagen": "imagenes/pikmin.png",
-        "url": "pikmin.iso"
+        "url": "arcadelocal/archivos_locales/pikmin.iso"
     },
     {
         "titulo": "Super Mario Sunshine",
         "consola": "gamecube",
         "imagen": "imagenes/sunshine.png",
-        "url": "sunshine.iso"
+        "url": "arcadelocal/archivos_locales/sunshine.iso"
     }
 ];
